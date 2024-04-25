b40e01f7ab118255dc0ee2aee760ae842fea9dbe
devarDilshad <devar.dilshad99@gmail.com>
2024-04-25 04:11:48 +0300

    added link for my portfolio

--- app/page.js
+++ app/page.js
@@ -10,9 +10,9 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-28 w-full items-end justify-center bg-gradient-to-t from-black via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none text-white flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://devar-dlshad.netlify.app/"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="author"
           >
             By Devar
           </a>
@@ -94,8 +94,8 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Calculate your age accurately. Instantly determine your age in years, months, and
-            days with just a few clicks.
+            Calculate your age accurately. Instantly determine your age in
+            years, months, and days with just a few clicks.
           </p>
         </a>
 
