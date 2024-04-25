65410ba48e7ccc7c0ef4a0ef7c05e7e040a72cfc
devarDilshad <devar.dilshad99@gmail.com>
2024-04-25 04:52:53 +0300

    added PWA as test

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,46 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "YouTility",
+  applicationName: "YouTility",
+  title: {
+    default: "YouTility",
+    template: "YouTility",
+  },
   description:
     "Explore our suite of handy online tools including discount calculator, password generator, age calculator, and more. Simplify your tasks effortlessly!",
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "YouTility",
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: "YouTility",
+    title: {
+      default: "YouTility",
+      template: "YouTility",
+    },
+    description:
+      "Explore our suite of handy online tools including discount calculator, password generator, age calculator, and more. Simplify your tasks effortlessly!",
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: "YouTility",
+      template: "YouTility",
+    },
+    description:
+      "Explore our suite of handy online tools including discount calculator, password generator, age calculator, and more. Simplify your tasks effortlessly!",
+  },
+};
+
+export const viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({ children }) {
